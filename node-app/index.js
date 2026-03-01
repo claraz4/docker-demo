ab82f81c5cd4892e460e4f85cdcacfd88b2b6c6f
Clara <clarazammar4@gmail.com>
2026-03-01 18:19:05 +0200

    Add react app without nginx + Test new backend APIs for the logs

--- node-app/index.js
+++ node-app/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req, res) => {
- res.send("Node container is running");
+app.use(cors());
+
+// to log the requests at every point
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 
-app.listen(3000, () => {
- console.log("Node app listening on port 3000");
+app.get("/", (req, res) => {
+  res.send("Node container is running");
 });
+
+app.get("/api1", (req, res) => {
+  res.send("api 1 response");
+});
+
+app.get("/api2", (req, res) => {
+  res.send("api 2 response");
+});
+
+app.listen(3000);
